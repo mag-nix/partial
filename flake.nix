3b808b6cc02330257609f753c278dc53eeae7c1a
Johannes Jeising <jeising@magazino.eu>
2024-11-06 16:36:53 +0100

    add option to not use upstream in ci

--- flake.nix
+++ flake.nix
@@ -10,16 +10,17 @@
   outputs = { self, nixpkgs, upstream }:
   let
     system = "x86_64-linux";
+    up = if builtins.pathExists ./local then upstream else null;
   in
   {
     inherit system;
     pkgs = import nixpkgs { inherit system; };
 
     packages.x86_64-linux = {
-      a = self.pkgs.callPackage (if builtins.pathExists ./a then ./a else upstream + "/a") { };
-      b = self.pkgs.callPackage (if builtins.pathExists ./b then ./b else upstream + "/b") { };
-      c = self.pkgs.callPackage (if builtins.pathExists ./c then ./c else upstream + "/c") { };
-      abc = self.pkgs.callPackage (if builtins.pathExists ./abc then ./abc else upstream + "/abc") {
+      a = self.pkgs.callPackage (if builtins.pathExists ./a then ./a else up + "/a") { };
+      b = self.pkgs.callPackage (if builtins.pathExists ./b then ./b else up + "/b") { };
+      c = self.pkgs.callPackage (if builtins.pathExists ./c then ./c else up + "/c") { };
+      abc = self.pkgs.callPackage (if builtins.pathExists ./abc then ./abc else up + "/abc") {
         a = self.packages.x86_64-linux.a;
         b = self.packages.x86_64-linux.b;
         c = self.packages.x86_64-linux.c;
