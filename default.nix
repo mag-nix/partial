45a273fbbaa2c473daa4e4c36ce818609d838bc3
Johannes Jeising <jeising@magazino.eu>
2024-11-06 11:49:44 +0100

    get a from upstream

--- default.nix
+++ default.nix
@@ -7,13 +7,13 @@ let
   }) {};
   upstream = import (builtins.fetchTarball {
     name = "upstream-repo";
-    url = "https://github.com/mag-nix/partial/.tar.gz";
-    sha256 = "";
+    url = "https://github.com/mag-nix/partial/archive/08beac640a63e2ac4ca40a8fdf72ce52dbf0e442.tar.gz";
+    sha256 = "sha256:0lsygcnvdzmvdnk6jl4rh429iswasvlx1768l1yny1732pnbqm2a";
   }) {};
 in
 {
   packages = rec {
-  a = if builtins.pathExists ./a then pkgs.callPackage ./a { } else upstream.packages.a;
+  a = if builtins.pathExists ./a then pkgs.callPackage ./a { } else upstream.packages.a; # renamed z so upstream is used
   b = if builtins.pathExists ./b then pkgs.callPackage ./b { } else upstream.packages.b;
   c = if builtins.pathExists ./c then pkgs.callPackage ./c { } else upstream.packages.c;
   abc = if builtins.pathExists ./abc then pkgs.callPackage ./abc {
