1d60da8c2f7ef9f6c597f73bd2d82710970a5376
Johannes Jeising <jeising@magazino.eu>
2024-11-06 12:01:39 +0100

    enable flakes

--- flake.nix
+++ flake.nix
@@ -3,23 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    upstream.url = "github:mag-nix/partial?ref=main";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, upstream }: {
     pkgs = import nixpkgs { system = "x86_64-linux"; };
+    upstream = import upstream { };
 
     packages.x86_64-linux = {
-      a = self.pkgs.callPackage ./a { };
-      b = self.pkgs.callPackage ./b { };
-      c = self.pkgs.callPackage ./c { };
-      abc = self.pkgs.callPackage ./abc {
+      a = if builtins.pathExists ./a then self.pkgs.callPackage ./a { } else upstream.packages.a; # renamed z so upstream is used
+      b = if builtins.pathExists ./b then self.pkgs.callPackage ./b { } else upstream.packages.b;
+      c = if builtins.pathExists ./c then self.pkgs.callPackage ./c { } else upstream.packages.c;
+      abc = if builtins.pathExists ./abc then self.pkgs.callPackage ./abc {
         a = self.packages.x86_64-linux.a;
         b = self.packages.x86_64-linux.b;
         c = self.packages.x86_64-linux.c;
-      };
+      } else upstream.packages.abc;
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.a;
+    packages.x86_64-linux.default = self.packages.x86_64-linux.abc;
 
   };
 }
